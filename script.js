cda983fe209ad9bb3b19d4d9476fb77acc1b374e
Amit Kumar <www.yadavanshiamit@gmail.com>
2025-09-25 08:33:38 +0530

    third  commit

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ const lastName="Kumar";
 let balance=500;
 const age=20;
 
+function updateBalance(value)
+{
+    balance+=value;
+    console.log("Balance udatee successfully");
+}
 
+console.log("payment intergration successfully");
+console.log("Aap log ab easily payment system use kar sakte hai");
